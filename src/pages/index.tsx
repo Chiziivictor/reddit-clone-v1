4d4b5b927b417b34120ed93c5d6460ce66307486
chiziivictor <chizivwokoma@gmail.com>
2022-06-30 05:53:23 +0100

    header component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+import Header from "../components/Header";
 import styles from "../styles/Home.module.css";
 
 const Home: NextPage = () => {
@@ -10,6 +11,8 @@ const Home: NextPage = () => {
         <title>Reddit Clone</title>
         <meta name="description" content="Reddit clone by @chiziivictor" />
       </Head>
+
+      <Header />
     </div>
   );
 };
