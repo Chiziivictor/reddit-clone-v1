2c45109eb0bd08966a056c308a167ee65040a6f8
chiziivictor <chizivwokoma@gmail.com>
2022-06-30 05:10:28 +0100

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,17 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Reddit Clone</title>
+        <meta name="description" content="Reddit clone by @chiziivictor" />
+      </Head>
+    </div>
+  );
+};
+
+export default Home;
